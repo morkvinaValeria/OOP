2dc998c9a64eb96c472d0086a8c206d1f0855412
morkvinaValeria <56650323+morkvinaValeria@users.noreply.github.com>
2020-03-26 11:34:24 +0200

    Add files via upload
    Class with indexator

---
+++ Lab3_class_with_indexator/Letters.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Letters
+    {
+
+        private char[,] array = new char[5, 5];
+        public void set(char[,] array)
+        {
+
+            this.array = array;
+        }
+        public char[,] get()
+        {
+            return this.array;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                char[] arr = new char[array.GetLength(1)]; 
+                for (int i = 0; i < array.GetLength(0); i++)
+                    arr[i] = array[index, i];
+                return new string(arr);
+            }
+        }
+
+        public int ConsonantsCount
+        {
+
+            get
+            {
+                string consonants = "bcdfghjklmnpqrstvwxz";
+                int count = 0;
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (consonants.Contains(array[i, j])) count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+
+        public void text_output()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+}
---
+++ Lab3_class_with_indexator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Lab3
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             char[,] matrix = new char[6, 6]
+             { {'a','b','c','d','e','f' },
+             { 'a','w','c','d','i','g'},
+             {'a','u','c','d','e','k' },
+             { 'a','b','y','d','e','r'},
+             { 'a','b','o','d','e','f'},
+             { 'a','p','c','d','e','f'} };
+            
+            Letters symbols = new Letters();
+            symbols.set(matrix);
+            symbols.text_output();
+            Console.WriteLine($"Третья строка массива: { symbols[2]}");
+            Console.WriteLine($"Кол-во согласных букв в массиве: {symbols.ConsonantsCount}");
+
+        }
+    }
+}
